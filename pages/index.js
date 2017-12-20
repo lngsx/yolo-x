444e2c2d160d2f2cf8174bd974246009f6b653ab
Luang Chuenjit <ntpat.ch@gmail.com>
2017-12-21 01:37:47 +0700

    เค
    - Install gravatar
    - update index page with Nav component and token uitls

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,50 @@
+import { Component } from 'react';
 import s from '../next_config/style';
+import Nav from '../components/Nav';
+import gravatar from 'gravatar';
+import {
+	composeAuthInfoHeaderFromLocalStorage,
+	validateLocalToken,
+	ifAuthPass
+} from '../utils';
 
-const IndexPage = () => (
-	<div className="sans-serif black-70">
-		<section className="mw7 center pa2">
-			<h1 className="f-subheadline measure">
+class IndexPage extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading: true,
+			isSignedIn: false,
+			user: {
+				email: '',
+				name: '',
+				avatar_url: ''
+			}
+		};
+	}
+
+	render() {
+		return (
+			<div className="sans-serif black-70">
+
+				<section className="mw7 center pa2">
+					<h1 className="f-subheadline measure">
 				A modular scale, like a musical scale, is a prearranged set of harmonious proportions.
 			</h1>
-			
-			<p className="measure lh-copy">
+
+					<p className="measure lh-copy">
 				Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Sed posuere consectetur est at lobortis.
 			</p>
-				
-			<p className="measure lh-copy">
+
+					<p className="measure lh-copy">
 				Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nulla vitae elit libero, a pharetra augue. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
 			</p>
 
-		</section>
+				</section>
 
-		<style jsx global>{ s }</style>
-	</div>
-);
+				<style jsx global>{ s }</style>
+			</div>
+		);
+	}
+}
 
 export default IndexPage;
